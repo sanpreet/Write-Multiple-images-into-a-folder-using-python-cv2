f396834f94ccc5c0810d0d377857b8ebed4274e8
Sanpreet Singh <sany26.com@gmail.com>
2021-01-02 22:18:17 +0530

    Update reading_writing_multiple_images_opencv2.py

--- reading_writing_multiple_images_opencv2.py
+++ reading_writing_multiple_images_opencv2.py
@@ -11,7 +11,7 @@ for bb,file in enumerate (glob.glob(path)):
     cv2.imshow('Color image', c)
     # writing the images in a folder output_images
     cv2.imwrite('A:\images\output_images\messigray{}.png'.format(bb), c)
-    #wait for 1 second
+    # wait for 1 second
     k = cv2.waitKey(1000)
-    #destroy the window
+    # destroy the window
     cv2.destroyAllWindows()
