3ff53d0a02461f4c74be3a08bc493311883d7200
Sanpreet Singh <sany26.com@gmail.com>
2021-01-02 22:17:53 +0530

    Update reading_writing_multiple_images_opencv2.py

--- reading_writing_multiple_images_opencv2.py
+++ reading_writing_multiple_images_opencv2.py
@@ -1,44 +1,17 @@
-#import the library opencv
+# import the library opencv
 import cv2
-#globbing utility.
+# globbing utility.
 import glob
-#select the path
-path = "A:\MY_company\Sanpreet_Singh\Client Work\OpenCV-Sanjeev\images\*.*"
+# select the path
+path = "A:\images\input_images\*.*"
 for bb,file in enumerate (glob.glob(path)):
-    print(bb,file)
-    a= cv2.imread(file)
-    print(a)
-    # %%%%%%%%%%%%%%%%%%%%%
-    #conversion numpy array into rgb image to show
-    c = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
+    image_read = cv2.imread(file)
+    # conversion numpy array into rgb image to show
+    c = cv2.cvtColor(image_read, cv2.COLOR_BGR2RGB)
     cv2.imshow('Color image', c)
-    #writing the images in a folder output_images
-    cv2.imwrite('A:\MY_company\Sanpreet_Singh\Client Work\OpenCV-Sanjeev\images\output_images\messigray{}.png'.format(bb), c)
+    # writing the images in a folder output_images
+    cv2.imwrite('A:\images\output_images\messigray{}.png'.format(bb), c)
     #wait for 1 second
     k = cv2.waitKey(1000)
     #destroy the window
     cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
